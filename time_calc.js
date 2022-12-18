77984434ecdcda49a14458db3df40aca6f72108b
Mait Jurask <maitj@tlu.ee>
2022-12-18 19:41:59 +0200

    TimeCalculations class

--- time_calc.js
+++ time_calc.js
@@ -1,5 +1,8 @@
+const {initializeApp} = require('firebase/app');
+const {getDatabase, get, ref, update} = require('firebase/database');
+
 class TimeCalculations {
-    start_time =  Date.now();
+    start_time = Date.now();
     arduino_millis = [0, 0]
     arduino_starts = [0, 0]
     yellow_states = [0, 0, 0, 0];
@@ -11,11 +14,42 @@ class TimeCalculations {
     greenWaveStartPos = 0;
 // Ehk laine järjekord, määratakse esimene foor.
 
-    constructor(cycle_length) {
-        this.cycle_length = cycle_length;
+    constructor() {
+        const firebaseConfig = {
+            apiKey: "AIzaSyBt0s4jGLD1k3_p8SfGS4yhoil0BKmZgKE",
+            authDomain: "budget-kahoot.firebaseapp.com",
+            databaseURL: "https://budget-kahoot-default-rtdb.europe-west1.firebasedatabase.app",
+            projectId: "budget-kahoot",
+            storageBucket: "budget-kahoot.appspot.com",
+            messagingSenderId: "332478696005",
+            appId: "1:332478696005:web:2b6adcec8ceeb90b4c312c"
+        };
+
+        const firebaseApp = initializeApp(firebaseConfig);
+        this.db = getDatabase(firebaseApp);
+
+        this.cycle_length = 20000;
+        const db_cycle_length_ref = ref(this.db, 'traffic_lights/cycleLength');
+        get(db_cycle_length_ref).then(snapshot => {
+            const cycle_length = snapshot.val();
+            this.cycle_length = cycle_length
+        })
     }
 
-    calculateArduinoMillis = (board_id) => {
+    calculateArduinoMillis = async (board_id) => {
+        const f_start_time = await this.getStartTime();
+        const f_arduino_starts = await this.getArduinoStarts();
+        const f_arduino_millis = await this.getArduinoMillis();
+
+
+        this.start_time = f_start_time;
+        this.arduino_millis = f_arduino_millis;
+        this.arduino_starts = f_arduino_starts;
+
+        console.log('f_start_time: ', this.start_time);
+        console.log('f_arduino_starts: ', this.arduino_millis);
+        console.log('f_arduino_millis: ', this.arduino_starts);
+
         const l_start_time = this.arduino_starts[board_id];
 
         console.log(`start_time: ${this.start_time}, l_start_time: ${l_start_time}`);
@@ -25,25 +59,41 @@ class TimeCalculations {
         // arduino_starts.forEach((l_start_time, i) => {
         //     console.log('l_start_time: ', l_start_time);
         // })
+
+        const new_millis = {
+            arduinoNextOffsets: this.arduino_millis
+        }
+        console.log('after updating: ', this.arduino_millis);
+        await this.setArduinoMillis(new_millis);
     }
 
     updateCycleTime = async () => {
+        const start_time = (await get(ref(this.db, 'traffic_lights/startTime/time'))).val();
+
+        console.log('fetched_start_time: ', start_time);
+        this.start_time = start_time;
+
         console.log('updateCycleTime');
         let current_time = Date.now();
         console.log('cycle_length: ', this.cycle_length);
 
-        if (current_time - this.start_time > 40000) {
-            const new_cycle = current_time + this.cycle_length;
-            this.start_time = new_cycle;
-            console.log('set start_time > 40000');
-            return;
-        }
 
         if (current_time > this.start_time) {
             const new_cycle = this.start_time + this.cycle_length;
             this.start_time = new_cycle;
             console.log('set start_time');
         }
+
+        if (current_time - this.start_time > 40000) {
+            const new_cycle = current_time + this.cycle_length;
+            this.start_time = new_cycle;
+            console.log('set start_time > 40000');
+        }
+
+        const new_time = {
+            time: this.start_time
+        }
+        await update(ref(this.db, 'traffic_lights/startTime'), new_time).catch((e) => console.error('update err: ', e));
     }
     cycleUpdateMiddleware = async (req, res, next) => {
         await this.updateCycleTime();
@@ -52,26 +102,36 @@ class TimeCalculations {
         next();
     }
 
-    getStartTime = () => {
-        return this.start_time;
+    getStartTime = async () => {
+        const start_time = (await get(ref(this.db, 'traffic_lights/startTime/time'))).val();
+        return start_time;
     }
-    getArduinoMillis = () => {
-        return this.arduino_millis;
+    getArduinoMillis = async () => {
+        const arduino_millis = (await get(ref(this.db, 'traffic_lights/arduinoNextOffsets'))).val();
+        return arduino_millis;
     }
-    getArduinoStarts = () => {
-        return this.arduino_starts;
+    getArduinoStarts = async () => {
+        const arduino_starts = (await get(ref(this.db, 'traffic_lights/arduinoStarts'))).val();
+        return arduino_starts;
     }
-    setArduinoStarts = (starts) => {
+    setArduinoStarts = async (starts) => {
+        console.log('starts: ', starts);
+        await update(ref(this.db, 'traffic_lights'), starts);
         this.arduino_starts = starts;
     }
+    setArduinoMillis = async (millis) => {
+        console.log('millis: ', millis);
+        await update(ref(this.db, 'traffic_lights'), millis);
+        this.arduino_millis = millis;
+    }
     getCycleLength = () => {
-        return this.cycleLength;
+        return this.cycle_length;
     }
     getYellowStates = () => {
         return this.yellow_states;
     }
     setCycleLength = (length) => {
-        this.cycleLength = length;
+        this.cycle_length = length;
     }
     setYellowState = (id, state) => {
         this.yellow_states[id] = state;
